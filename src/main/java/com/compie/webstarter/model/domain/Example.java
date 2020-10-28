363cf666f7eb077cab8a9fb89a1ff58b01b19c4b
Jacob Nathan <yaacovisraelnathan@gmail.com>
2020-10-28 12:37:12 +0200

    lombok dynamic getter and setter to string and equal and hash code compare function by adding the "data" annotation

--- src/main/java/com/compie/webstarter/model/domain/Example.java
+++ src/main/java/com/compie/webstarter/model/domain/Example.java
@@ -1,10 +1,15 @@
 package com.compie.webstarter.model.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@EqualsAndHashCode
 @Entity(name = "example")
 public class Example {
     @Id
@@ -14,28 +19,4 @@ public class Example {
     private String name;
 
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
