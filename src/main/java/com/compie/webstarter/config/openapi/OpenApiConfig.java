88e7cb00e5d5fe74c0d910d32cb01d27689cc80c
Jacob Nathan <yaacovisraelnathan@gmail.com>
2020-10-28 11:23:56 +0200

    user repository

--- src/main/java/com/compie/webstarter/config/openapi/OpenApiConfig.java
+++ src/main/java/com/compie/webstarter/config/openapi/OpenApiConfig.java
@@ -1,21 +1,8 @@
 package com.compie.webstarter.config.openapi;
 
-//import com.google.common.base.Predicate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-
 class OpenApiConfig {
-
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.any())
-//                .paths((Predicate<String>) PathSelectors.any())
-//                .build();
-//    }
-
 
 }
